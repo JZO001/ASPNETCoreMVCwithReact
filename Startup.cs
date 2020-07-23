96171669d12c5aa1336ba435f7795a86a43ed496
Zoltan Juhasz <jzo@jzo.hu>
2020-07-23 13:57:49 +0200

    Catch error in dev mode also

--- Startup.cs
+++ Startup.cs
@@ -50,15 +50,10 @@ namespace ASPNETCoreMVCwithReact
 
             // handling error before IIS catch them
             app.UseStatusCodePagesWithReExecute("/Home/Errors/{0}");
+            app.UseExceptionHandler("/Home/Error");
 
-
-            if (env.IsDevelopment())
+            if (!env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
